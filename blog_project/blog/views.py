397f4a38df385fb772fd41a787034e6748da41ba
MohammadKhaledKassab <mohamedkhaledkassab@gmail.com>
2020-03-27 15:49:57 +0100

    blog Project with Forms

--- blog_project/blog/views.py
+++ blog_project/blog/views.py
@@ -1,5 +1,7 @@
 from .models import Post
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView, DetailView #, CreateView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy # new
 
 class BlogListView(ListView):
     model=Post
@@ -8,3 +10,18 @@ class BlogListView(ListView):
 class BlogDetailView(DetailView):
     model=Post
     template_name='post_detail.html'
+
+class BlogCreateView(CreateView):
+    model=Post
+    template_name='post_new.html'
+    fields='__all__'
+    
+class BlogUpdateView(UpdateView):
+    model = Post
+    template_name = 'post_edit.html'
+    fields = ['title', 'body']
+    
+class BlogDeleteView(DeleteView):
+    model=Post
+    template_name='post_delete.html'
+    success_url=reverse_lazy('home')
