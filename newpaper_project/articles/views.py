5ef565813dfda8627b742bc92eb83bbb05488d6e
MohammadKhaledKassab <mohamedkhaledkassab@gmail.com>
2020-04-16 12:17:24 +0200

    authentication

--- newpaper_project/articles/views.py
+++ newpaper_project/articles/views.py
@@ -1,3 +1,7 @@
+from django.contrib.auth.mixins import LoginRequiredMixin
+
+from django.core.exceptions import PermissionDenied
+
 from django.views.generic import ListView, DetailView
 
 from django.views.generic.edit import UpdateView, DeleteView, CreateView
@@ -6,25 +10,47 @@ from django.urls import reverse_lazy # new
 
 from .models import Article
 
-class ArticleListView(ListView):
+class ArticleListView(LoginRequiredMixin, ListView):
     model = Article
     template_name = 'article_list.html'
+    login_url='login'
     
-class ArticleDetailView(DeleteView):
+class ArticleDetailView(LoginRequiredMixin, DetailView):
     model=Article
     template_name='article_detail.html'
+    login_url='login'
     
-class ArticleUpdateView(UpdateView): # new
+class ArticleUpdateView(LoginRequiredMixin, UpdateView): # new
     model = Article
     fields = ('title', 'body',)
     template_name = 'article_edit.html'
+    login_url='login'
+    
+    def dispatch(self, request, *args, **kwargs):
+        obj=self.get_object()
+        if obj.author != self.request.user:
+            raise PermissionDenied
+        return super.dispatch(self, request, *args, **kwargs)
+        
 
-class ArticleDeleteView(DeleteView): # new
+class ArticleDeleteView(LoginRequiredMixin, DeleteView): # new
     model = Article
     template_name = 'article_delete.html'
     success_url = reverse_lazy('article_list')
+    login_url='login'
     
-class ArticleCreateView(CreateView):
+    def dispatch(self, request, *args, **kwargs): # new
+        obj = self.get_object()
+        if obj.author != self.request.user:
+            raise PermissionDenied
+        return super().dispatch(request, *args, **kwargs)
+    
+class ArticleCreateView(LoginRequiredMixin, CreateView):
     model = Article
     template_name = 'article_new.html'
-    fields=('title', 'body', 'author',)
+    fields=('title', 'body', )
+    login_url='login'
+    
+    def form_valid(self, form):
+        form.instance.author=self.request.user
+        return super().form_valid(form)
